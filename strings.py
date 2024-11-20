42cbdc491f9be5cc915316d9b2fa3e085af93871
Gaurav Devrani <gauravdevrani60@gmail.com>
2024-11-20 10:46:46 +0530

    basics of python

---
+++ strings.py
@@ -0,0 +1,13 @@
+# Question: 1
+# Write a Python program to concatenate the strings "Hello" and "World!" with a space in between.
+
+str1 ="hello"
+str2 = "world"
+print(str1 + " " + str2)
+
+# Question: 2
+# Given the string s = "PythonProgramming", extract the substring "Programming" using slicing.
+
+s = "PythonProgramming"
+substring = s[6:]  # Start from index 6 to the end
+print(substring)
